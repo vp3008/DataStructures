2fd1d002782623fbcbb4efd81afb053f7c5f6dc2
deepankar.pathak <vp3008@gmail.com>
2020-03-03 14:51:51 +0530

    Added right rotation

--- Data Structures/src/Arrays/Rotation/SimpleRotation.java
+++ Data Structures/src/Arrays/Rotation/SimpleRotation.java
@@ -17,16 +17,37 @@ public class SimpleRotation
         }
         System.out.println ("Rotate by");
         int d = scan.nextInt ();
-        rotateArray(arr, d);
+        if(d>0)
+            rotateArrayLeft(arr, d);
+        if(d<0)
+            rotateArrayRight(arr, -d);
         System.out.println (Arrays.toString (arr));
     }
 
-    public static void rotateArray(int[] arr, int d)
+    public static void rotateArrayLeft(int[] arr, int d)
     {
+        System.out.println ("Left Rotation");
         int n = arr.length;
         int[] temp = new int[d];
+        //copying the first 'd' elements into the temp array
         System.arraycopy (arr, 0, temp, 0, d);
+        //shifting the last 'n-d' elements to the front 'd' positions
         if (n - d >= 0) System.arraycopy (arr, d, arr, 0, n - d);
+        //copying the first 'd' elements back into the array
         System.arraycopy (temp, 0, arr, n-d, d);
+    }
+
+    public static void rotateArrayRight(int[] arr, int d)
+    {
+        System.out.println ("Right rotation");
+        int n = arr.length;
+        int[] temp = new int[d];
+        //copying last 'd' elements into the temp array
+        System.arraycopy (arr, n-d, temp, 0, d);
+        //shifting forward the first 'n-d' elements by d positions
+        System.arraycopy (arr, 0, arr, d, n-d);
+        //copying back the elements from the temp array into the array
+        System.arraycopy (temp, 0, arr, 0, d);
+
     }
 }
